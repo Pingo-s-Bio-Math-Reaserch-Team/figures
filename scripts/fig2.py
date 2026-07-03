ef5c4c2022e60175e99d1c82aaf1185f27a704b8
donutaskwhy787-cmyk <donutaskwhy787@gmail.com>
2026-07-03 19:46:03 -0400

    here rain heres some help

--- scripts/fig2.py
+++ scripts/fig2.py
@@ -11,18 +11,42 @@ df = pd.read_csv("data\source_tables\performance_mean_sd_for_latex.csv")
 t = list(df['Task'])
 tmp = set()
 
-for str in t:
-    if not(str in tmp):
-        tmp.add(str)
+for st in t:
+    if not(st in tmp):
+        tmp.add(st)
         
 tasks = tuple(tmp)
+baseline = df.loc[df['Model'] == "Baseline"]
+sheaf = df.loc[df['Model'] == "Sheaf"]
+
+baseValList = list()
+baseErrorList = list()
+sheafValList = list()
+sheafErrorList = list()
+
+for st in tasks:
+    bs = baseline.loc[df['Task'] == str(st)]
+    bstr = bs.iloc[0]['Balanced accuracy']
     
+    bfl = float(bstr[0:bstr.find("$")])
+    baseValList.append(bfl)
+    
+    ebfl = float(bstr[bstr.find("$")+6:])
+    baseErrorList.append(ebfl)
+    
+    ss = sheaf.loc[df['Task'] == str(st)]
+    sstr = ss.iloc[0]['Balanced accuracy']
+    
+    sfl = float(sstr[0:sstr.find("$")])
+    sheafValList.append(sfl)
+    
+    esfl = float(sstr[sstr.find("$")+6:])
+    sheafErrorList.append(esfl)
 
-
-penguin_means = {
-    'Bill Depth': (18.35, 18.43, 14.98),
-    'Bill Length': (38.79, 48.83, 47.50),
-    'Flipper Length': (189.95, 195.82, 217.19),
+  
+data = {
+    'Baseline': baseValList,
+    'Sheaf': sheafValList,
 }
 
 x = np.arange(len(tasks))  # the label locations
@@ -31,7 +55,7 @@ multiplier = 0
 
 fig, (ax1, ax2) = plt.subplots(1, 2, layout='constrained')
 fig.set_size_inches(10, 6)
-for attribute, measurement in penguin_means.items():
+for attribute, measurement in data.items():
     offset = width * multiplier
     rects = ax1.bar(x + offset, measurement, width, label=attribute)
     ax1.bar_label(rects, padding=3)
@@ -42,6 +66,7 @@ ax1.set_ylabel('Length (mm)')
 ax1.set_title('Penguin attributes by species')
 ax1.set_xticks(x + width, tasks)
 ax1.legend(loc='upper left', ncols=3)
-ax1.set_ylim(0, 250)
+ax1.set_ylim(0, 1)
 
 plt.show()
+  
