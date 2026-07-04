fc6d4e8dc3979d7c438cb3490bcb6964fd3354f7
donutaskwhy787-cmyk <donutaskwhy787@gmail.com>
2026-07-04 14:09:12 -0400

    axews

--- scripts/fig3.py
+++ scripts/fig3.py
@@ -8,6 +8,12 @@ from pathlib import Path
 
 df = pd.read_csv("iteration2_patient_transition_risk_scores.csv")
 
+fig, (ax1, ax2) = plt.subplots(1, 2, layout='constrained')
+fig.set_size_inches(10, 4)
+
+
+
+
 plt.tight_layout()
 plt.savefig('fig4.png', dpi=300, bbox_inches='tight')
 plt.show()
