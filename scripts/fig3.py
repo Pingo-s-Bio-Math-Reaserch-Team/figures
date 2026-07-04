d2049e928288109fc96f51145b6f83567c855271
Leo Su <leo.q.su@gmail.com>
2026-07-04 14:31:48 -0400

    idk something

--- scripts/fig3.py
+++ scripts/fig3.py
@@ -4,9 +4,11 @@ import numpy as np
 import pandas as pd
 import os 
 from pathlib import Path
+from sklearn.decomposition import PCA
 
-
-df = pd.read_csv("data/final_competitive_results/iteration2_patient_transition_risk_scores.csv")
+pat = pd.read_csv("data/final_competitive_results/iteration2_patient_transition_risk_scores.csv")
+adj = pd.read_csv("data/final_competitive_results/iteration2_transition_adjusted_cox.csv")
+km = pd.read_csv("data/final_competitive_results/iteration2_transition_km_logrank.csv")
 
 fig, (ax1, ax2) = plt.subplots(1, 2, layout='constrained')
 fig.set_size_inches(10, 4)
@@ -15,5 +17,5 @@ fig.set_size_inches(10, 4)
 
 
 plt.tight_layout()
-plt.savefig('fig4.png', dpi=300, bbox_inches='tight')
+plt.savefig('fig3.png', dpi=300, bbox_inches='tight')
 plt.show()
